ec00143be5fdb53e6a83b247fab373ade77ed40a
zumehasi1019 <zumehasi.takaya1019@gmail.com>
2017-10-15 19:48:50 +0900

    Capistrano

--- Gemfile
+++ Gemfile
@@ -23,6 +23,11 @@ group :development, :test do
   gem 'factory_girl_rails', "~> 4.4.1"
   gem 'faker'
   gem 'rails-controller-testing'
+  gem 'capistrano'
+  gem 'capistrano-rbenv'
+  gem 'capistrano-bundler'
+  gem 'capistrano-rails'
+  gem 'capistrano3-unicorn'
 end
 
 group :development do
---
+++ config/deploy.rb
@@ -0,0 +1,24 @@
+# config valid only for current version of Capistrano
+lock "3.9.1"
+
+set :application, 'newspicks'
+set :repo_url,  'git@github.com:zumehas1019/newspicks.git'
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.3.1'
+
+set :ssh_options, auth_methods: ['publickey'],
+                  keys: ['<ローカルPCのEC2インスタンスのSSH鍵(pem)へのパス>']  ※例：/Users/yusukeyamane/.ssh/key_pem.pem
+
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
