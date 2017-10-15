4b76d0824a9cee92d6825a80ad619c1578fa6b17
zumehasi1019 <zumehasi.takaya1019@gmail.com>
2017-10-15 21:07:48 +0900

    テスト

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['<ローカルPCのEC2インスタンスのSSH鍵(pem)へのパス>']  ※例：/Users/yusukeyamane/.ssh/key_pem.pem
+                  keys: ['/Users/takaya/.ssh/hashizume.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
